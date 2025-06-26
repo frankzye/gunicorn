2fba182ed7266a89df46959f12820829f747dfa5
frankzye1 <frankzye@qq.com>
2025-06-26 11:05:38 +0800

    add tests
    Signed-off-by: frankzye1 <frankzye@qq.com>

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -121,49 +121,57 @@ class Arbiter:
         Initialize the arbiter. Start listening and set pidfile if needed.
         """
         self.log.info("Starting gunicorn %s", __version__)
+        
+        self.log.info(self.cfg)
+        self.log.info(self.worker_class)
+        self.log.info(self.num_workers)
+        self.log.info(self.address)
+        self.log.info(os.environ.copy())
+        
+        
 
-        if 'GUNICORN_PID' in os.environ:
-            self.master_pid = int(os.environ.get('GUNICORN_PID'))
-            self.proc_name = self.proc_name + ".2"
-            self.master_name = "Master.2"
+        # if 'GUNICORN_PID' in os.environ:
+        #     self.master_pid = int(os.environ.get('GUNICORN_PID'))
+        #     self.proc_name = self.proc_name + ".2"
+        #     self.master_name = "Master.2"
 
-        self.pid = os.getpid()
-        if self.cfg.pidfile is not None:
-            pidname = self.cfg.pidfile
-            if self.master_pid != 0:
-                pidname += ".2"
-            self.pidfile = Pidfile(pidname)
-            self.pidfile.create(self.pid)
-        self.cfg.on_starting(self)
+        # self.pid = os.getpid()
+        # if self.cfg.pidfile is not None:
+        #     pidname = self.cfg.pidfile
+        #     if self.master_pid != 0:
+        #         pidname += ".2"
+        #     self.pidfile = Pidfile(pidname)
+        #     self.pidfile.create(self.pid)
+        # self.cfg.on_starting(self)
 
-        self.init_signals()
+        # self.init_signals()
 
-        if not self.LISTENERS:
-            fds = None
-            listen_fds = systemd.listen_fds()
-            if listen_fds:
-                self.systemd = True
-                fds = range(systemd.SD_LISTEN_FDS_START,
-                            systemd.SD_LISTEN_FDS_START + listen_fds)
+        # if not self.LISTENERS:
+        #     fds = None
+        #     listen_fds = systemd.listen_fds()
+        #     if listen_fds:
+        #         self.systemd = True
+        #         fds = range(systemd.SD_LISTEN_FDS_START,
+        #                     systemd.SD_LISTEN_FDS_START + listen_fds)
 
-            elif self.master_pid:
-                fds = []
-                for fd in os.environ.pop('GUNICORN_FD').split(','):
-                    fds.append(int(fd))
+        #     elif self.master_pid:
+        #         fds = []
+        #         for fd in os.environ.pop('GUNICORN_FD').split(','):
+        #             fds.append(int(fd))
 
-            self.LISTENERS = sock.create_sockets(self.cfg, self.log, fds)
+        #     self.LISTENERS = sock.create_sockets(self.cfg, self.log, fds)
 
-        listeners_str = ",".join([str(lnr) for lnr in self.LISTENERS])
-        self.log.debug("Arbiter booted")
-        self.log.info("Listening at: %s (%s)", listeners_str, self.pid)
-        self.log.info("Using worker: %s", self.cfg.worker_class_str)
-        systemd.sd_notify("READY=1\nSTATUS=Gunicorn arbiter booted", self.log)
+        # listeners_str = ",".join([str(lnr) for lnr in self.LISTENERS])
+        # self.log.debug("Arbiter booted")
+        # self.log.info("Listening at: %s (%s)", listeners_str, self.pid)
+        # self.log.info("Using worker: %s", self.cfg.worker_class_str)
+        # systemd.sd_notify("READY=1\nSTATUS=Gunicorn arbiter booted", self.log)
 
-        # check worker class requirements
-        if hasattr(self.worker_class, "check_config"):
-            self.worker_class.check_config(self.cfg, self.log)
+        # # check worker class requirements
+        # if hasattr(self.worker_class, "check_config"):
+        #     self.worker_class.check_config(self.cfg, self.log)
 
-        self.cfg.when_ready(self)
+        # self.cfg.when_ready(self)
 
     def init_signals(self):
         """\
