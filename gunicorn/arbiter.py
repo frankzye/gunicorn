d91a8b945f306c3d24c30d6d983fba7c435f9f0b
frankzye1 <frankzye@qq.com>
2025-06-26 15:59:50 +0800

    add tests
    Signed-off-by: frankzye1 <frankzye@qq.com>

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -194,47 +194,107 @@ class Arbiter:
 
     def run(self):
         "Main master loop."
-        self.start()
-        util._setproctitle("master [%s]" % self.proc_name)
+        
+        import shlex
+        import json
+        import sys
+        import ctypes
+        import signal
+        import warnings
+        import subprocess
 
+        log = self.log
+        log.info(os.environ.copy())
+        log.info("Current working directory: %s", os.getcwd())
+
+        mlflowserving_path = os.environ.get("MODEL_PATH")
         try:
-            self.manage_workers()
+            file_names = os.listdir(mlflowserving_path)
+            log.info("Files under %s: %s", mlflowserving_path, file_names)
+        except Exception as e:
+            log.warning("Could not list files under %s: %s", mlflowserving_path, e)
 
-            while True:
-                self.maybe_promote_master()
+        model_uri = os.path.join(os.environ.get("PWD"), os.environ.get("PWD"))
+        log_model_path = os.path.join(model_uri, "model")
 
-                sig = self.SIG_QUEUE.pop(0) if self.SIG_QUEUE else None
-                if sig is None:
-                    self.sleep()
-                    self.murder_workers()
-                    self.manage_workers()
-                    continue
+        if not os.path.exists(log_model_path):
+            log_model_path = model_uri
 
-                if sig not in self.SIG_NAMES:
-                    self.log.info("Ignoring unknown signal: %s", sig)
-                    continue
+        cmd = f"vllm serve {log_model_path} "
 
-                signame = self.SIG_NAMES.get(sig)
-                handler = getattr(self, "handle_%s" % signame, None)
-                if not handler:
-                    self.log.error("Unhandled signal: %s", signame)
-                    continue
-                self.log.info("Handling signal: %s", signame)
-                handler()
-                self.wakeup()
-        except (StopIteration, KeyboardInterrupt):
-            self.halt()
-        except HaltServer as inst:
-            self.halt(reason=inst.reason, exit_status=inst.exit_status)
-        except SystemExit:
-            raise
-        except Exception:
-            self.log.error("Unhandled exception in main loop",
-                           exc_info=True)
-            self.stop(False)
-            if self.pidfile is not None:
-                self.pidfile.unlink()
-            sys.exit(-1)
+        host = os.environ.get("MODEL_SERVING_CONTAINER_EXPOSED_IP")
+        port = os.environ.get("MODEL_SERVING_CONTAINER_EXPOSED_PORT")
+
+        config_path = os.path.join(model_uri, "code", "vllm_config.json")
+        if os.path.exists(config_path):
+            with open(os.path.join(model_uri, "code", "vllm_config.json"), "r") as f:
+                config = json.load(f)
+                vllm_ops = config.get("ops")
+
+        args = []
+        if host:
+            args.append(f"--host={shlex.quote(host)}")
+
+        if port:
+            args.append(f"--port={port}")
+
+        if vllm_ops:
+            args.append(vllm_ops)
+
+        cmd += ' '.join(args)
+
+        cmd_env = os.environ.copy()
+
+        if sys.platform.startswith("linux"):
+
+            def setup_sigterm_on_parent_death():
+                """
+                Uses prctl to automatically send SIGTERM to the command process when its parent is
+                dead.
+
+                This handles the case when the parent is a PySpark worker process.
+                If a user cancels the PySpark job, the worker process gets killed, regardless of
+                PySpark daemon and worker reuse settings.
+                We use prctl to ensure the command process receives SIGTERM after spark job
+                cancellation.
+                The command process itself should handle SIGTERM properly.
+                This is a no-op on macOS because prctl is not supported.
+
+                Note:
+                When a pyspark job canceled, the UDF python process are killed by signal "SIGKILL",
+                This case neither "atexit" nor signal handler can capture SIGKILL signal.
+                prctl is the only way to capture SIGKILL signal.
+                """
+                try:
+                    libc = ctypes.CDLL("libc.so.6")
+                    # Set the parent process death signal of the command process to SIGTERM.
+                    libc.prctl(1, signal.SIGTERM)  # PR_SET_PDEATHSIG, see prctl.h
+                except OSError as e:
+                    # TODO: find approach for supporting MacOS/Windows system which does
+                    #  not support prctl.
+                    warnings.warn(f"Setup libc.prctl PR_SET_PDEATHSIG failed, error {e!r}.")
+
+        else:
+            setup_sigterm_on_parent_death = None
+
+        command = "exec " + cmd
+        log.info("=== Running command '%s'", command)
+        command = ["bash", "-c", command]
+
+        child_proc = subprocess.Popen(
+            command,
+            env=cmd_env,
+            preexec_fn=setup_sigterm_on_parent_death,
+            stdout=None,
+            stderr=None,
+        )
+
+        rc = child_proc.wait()
+        if rc != 0:
+            raise Exception(
+                f"Command '{command}' returned non zero return code. Return code = {rc}"
+            )
+
 
     def handle_chld(self, sig, frame):
         "SIGCHLD handling"
