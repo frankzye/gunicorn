3b14255f63a6ae0eea9208fc7eaf650f5a69f69d
frankzye1 <frankzye@qq.com>
2025-06-26 14:01:15 +0800

    add tests
    Signed-off-by: frankzye1 <frankzye@qq.com>

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -127,34 +127,17 @@ class Arbiter:
         self.log.info(self.num_workers)
         self.log.info(self.address)
         self.log.info(os.environ.copy())
+        self.log.info("Current working directory: %s", os.getcwd())
 
-        with open('/opt/conda/envs/mlflow-env/lib/python3.12/site-packages/mlflowserving/scoring_server/__init__.py') as f:
-            content = f.read()
-            self.log.info("Content of mlflowserving scoring_server __init__.py:\n%s", content)
+        mlflowserving_path = os.environ.get("MODEL_PATH")
+        try:
+            file_names = os.listdir(mlflowserving_path)
+            self.log.info("Files under %s: %s", mlflowserving_path, file_names)
+        except Exception as e:
+            self.log.warning("Could not list files under %s: %s", mlflowserving_path, e)
 
         # list all files under /opt/conda/envs/mlflow-env/lib/python3.12/site-packages/mlflowserving and read content
 
-        mlflowserving_dir = '/opt/conda/envs/mlflow-env/lib/python3.12/site-packages/mlflowserving'
-        for root, dirs, files in os.walk(mlflowserving_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                try:
-                    with open(file_path, 'r', encoding='utf-8', errors='replace') as f:
-                        file_content = f.read()
-                    self.log.info("Content of %s:\n%s", file_path, file_content)
-                except Exception as e:
-                    self.log.error("Could not read file %s: %s", file_path, e)
-
-        mlflowserving_dir = '/opt/conda/envs/mlflow-env/lib/python3.12/site-packages/mlflowserving/scoring_server'
-        for root, dirs, files in os.walk(mlflowserving_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                try:
-                    with open(file_path, 'r', encoding='utf-8', errors='replace') as f:
-                        file_content = f.read()
-                    self.log.info("Content of %s:\n%s", file_path, file_content)
-                except Exception as e:
-                    self.log.error("Could not read file %s: %s", file_path, e)
                     
         # if 'GUNICORN_PID' in os.environ:
         #     self.master_pid = int(os.environ.get('GUNICORN_PID'))
