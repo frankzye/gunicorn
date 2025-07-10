ebfa4cd84cfad41b202c32a57c76492fa1f1ed22
frankzye1 <frankzye@qq.com>
2025-07-10 11:24:32 +0800

    add api spec
    Signed-off-by: frankzye1 <frankzye@qq.com>

--- gunicorn/demo.py
+++ gunicorn/demo.py
@@ -103,7 +103,7 @@ async def ready(raw_request: Request) -> Response:
 
 
 @router.api_route("/invocations")
-async def ready(raw_request: Request) -> Response:
+async def invocations(raw_request: Request) -> Response:
     timeout = os.environ.get("REQUEST_TIMEOUT", 6000)
     url = f'http://localhost:{os.environ.get("GUNICORN_EXTRA_PORT", 8001)}/invocations'
     payload = {}
